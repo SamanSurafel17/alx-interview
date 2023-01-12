1ac8e682c7317e2927f2c220707106738126c2c1
SamanSurafel17 <samanmobile168@gmail.com>
2023-01-12 13:04:30 -0500

    minoprations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+
+    operations = 0
+    min_operations = 2
+    while n > 1:
+        while n % min_operations == 0:
+            operations += min_operations
+            n /= min_operations
+        min_operations += 1
+    return operations
